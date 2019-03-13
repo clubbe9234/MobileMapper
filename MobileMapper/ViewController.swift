9e97f93fa5b8db4ded534a6103e39e18afa984b5
Caroline Lubbe <clubbe9234@jh-mba158-09.local>
2019-03-13 11:41:03 -0500

    map that searches for parks, lists the phone number, visits the website, and can zoom back out

--- MobileMapper/ViewController.swift
+++ MobileMapper/ViewController.swift
@@ -8,16 +8,109 @@
 
 import UIKit
 import MapKit
+import SafariServices
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
     @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    var currentLocation: CLLocation!
+    var parks: [MKMapItem] = []
+    var initialRegion: MKCoordinateRegion!
+    var isInitialMapLoad = true
+    
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+        if isInitialMapLoad {
+            initialRegion = MKCoordinateRegion(center: mapView.centerCoordinate, span: mapView.region.span)
+            isInitialMapLoad = false
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        locationManager.delegate = self
+        mapView.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations[0]
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation.isEqual(mapView.userLocation) {
+            return nil
+        } else {
+            let pin = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+            pin.image = UIImage(named: "tree")
+            pin.canShowCallout = true
+            let buttonOne = UIButton(type: .detailDisclosure)
+            pin.rightCalloutAccessoryView = buttonOne
+            let buttonTwo = UIButton(type: .contactAdd)
+            pin.leftCalloutAccessoryView = buttonTwo
+            return pin
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        let buttonPressed = control as! UIButton
+        var currentMapItem = MKMapItem()
+        if let title = view.annotation?.title, let parkName = title {
+            for mapItem in parks {
+                if mapItem.name == parkName {
+                    currentMapItem = mapItem
+                }
+            }
+        }
+        let placemark = currentMapItem.placemark
+        print(placemark)
+        if buttonPressed.buttonType == .detailDisclosure {
+            if let url = currentMapItem.url {
+                let svc = SFSafariViewController(url: url)
+                present(svc, animated: true, completion: nil)
+            }
+        } else {
+            mapView.setRegion(initialRegion, animated: true)
+            return
+        }
+    }
+    
+    @IBAction func whenZoomButtonPressed(_ sender: Any) {
+        let center = currentLocation.coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    @IBAction func whenSearchButtonPressed(_ sender: Any) {
+        var description = ""
+        let center = currentLocation.coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "Parks"
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            guard let response = response else {
+                return
+            }
+            for mapItem in response.mapItems {
+                self.parks.append(mapItem)
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = mapItem.placemark.coordinate
+                annotation.title = mapItem.name
+                if let phoneNumber = mapItem.phoneNumber {
+                    description += "Phone number: \(phoneNumber)\n"
+                }
+                annotation.subtitle = description
+                self.mapView.addAnnotation(annotation)
+                
+            }
+        }
+    }
 }
 
